9123b3c23f424aaae2cdd31d4dc94be55cba56f9
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-10-24 15:13:59 +1100

    exercises 04_traits/01_trait: fix grammar in docstring

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -1,4 +1,4 @@
-// Define a trait named `IsEven` that has a method `is_even` that returns a `true` if `self` is
+// Define a trait named `IsEven` that has a method `is_even` that returns `true` if `self` is
 // even, otherwise `false`.
 //
 // Then implement the trait for `u32` and `i32`.
