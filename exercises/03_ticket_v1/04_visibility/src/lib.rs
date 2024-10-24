a35c84b6e29834ed4deff90839ea9c4a92281d8b
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-10-24 11:27:45 +1100

    solve `03_ticket_v1/04_visibility`

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,5 +1,7 @@
 mod ticket {
-    struct Ticket {
+    #![allow(dead_code)]
+
+    pub struct Ticket {
         title: String,
         description: String,
         status: String,
@@ -36,6 +38,8 @@ mod ticket {
 //  in this exercise.
 #[cfg(test)]
 mod tests {
+    #![allow(unused_imports, dead_code)]
+
     // TODO: Add the necessary `pub` modifiers in the parent module to remove the compiler
     //  errors about the use statement below.
     use super::ticket::Ticket;
@@ -44,7 +48,7 @@ mod tests {
     // visibility to make the use statement compile!
     // Once you have verified that it indeed doesn't compile, comment it out.
     fn should_not_be_possible() {
-        let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
+        // let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
 
         // You should be seeing this error when trying to run this exercise:
         //
@@ -55,7 +59,7 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the line out to move on to the next exercise!
-        assert_eq!(ticket.description, "A description");
+        // assert_eq!(ticket.description, "A description");
     }
 
     fn encapsulation_cannot_be_violated() {
@@ -68,10 +72,10 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
+        // let ticket = Ticket {
+        //     title: "A title".into(),
+        //     description: "A description".into(),
+        //     status: "To-Do".into(),
+        // };
     }
 }
